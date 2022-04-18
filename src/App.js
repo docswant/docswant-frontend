a1b90be61bfa98fbfffc4d8f6a5f451104e2a6f7
Dev-jwJeong <rgn95652@gmail.com>
2022-04-18 23:57:36 +0900

    Fix : register url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     //컴포넌트 경로 설정
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
