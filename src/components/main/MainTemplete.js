a46dda8b506a8d04a46b07cf87078c600e4eb43a
Dev-jwJeong <rgn95652@gmail.com>
2022-04-20 21:31:05 +0900

    [Fix] :Link 충돌

--- src/components/main/MainTemplete.js
+++ src/components/main/MainTemplete.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
-import { Link } from 'react-router-dom';
 
 const MainTempleteBlock = styled.div`
   width: 100%;
