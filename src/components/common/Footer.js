24984c9736d9905bbfd44cdf079892e8ba20934b
Yujin830 <rladbwls30@naver.com>
2022-04-22 14:34:41 +0900

    FIX: footer수정

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,7 +6,7 @@ import { SiNotion } from 'react-icons/si';
 
 //화면 하단 고정
 const FooterBlock = styled.div`
-  position: absolute;
+  position: flex;
   width: 100%;
   bottom: 0;
   border-top: 1px solid ${palette.gray[0]};
