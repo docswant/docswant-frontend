fa01cb9a221b81ee0635b9e585e920a7c9e4d3d6
Dev-jwJeong <rgn95652@gmail.com>
2022-04-22 14:41:57 +0900

    Fix : login prettier

--- src/components/log/Login.js
+++ src/components/log/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -6,24 +6,24 @@ const LoginBlock = styled.div`
   padding: 20px 0 100px;
   background-color: ${palette.green};
   height: 550px;
-  h1{
+  h1 {
     color: black;
     font-size: 35px;
     font-weight: 700;
     text-align: center;
     margin-bottom: 70px;
   }
-  .login_box{
+  .login_box {
     text-align: center;
     width: 600px;
     margin: 0 auto;
     border-radius: 6px;
     background-color: white;
-    box-shadow: 2px 2px 20px rgba(0,0,0,0.3);
+    box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.3);
     color: #555;
     form {
       padding: 30px 22px;
-      input{
+      input {
         width: 100%;
         margin-bottom: 12px;
         padding: 15px;
@@ -32,7 +32,7 @@ const LoginBlock = styled.div`
         outline: none;
         box-sizing: border-box;
       }
-      [type="submit"]{
+      [type='submit'] {
         width: 150px;
         margin-top: 12px;
         border: none;
@@ -41,7 +41,7 @@ const LoginBlock = styled.div`
         cursor: pointer;
         background-color: #006633;
       }
-      p{
+      p {
         font-size: 15px;
         color: #006633;
       }
@@ -55,17 +55,17 @@ function Login() {
       <h1>로 그 인</h1>
       <div class="login_box">
         <form>
-          <input type="text" placeholder='아이디를 입력하세요.' />
-          <input type="password" placeholder='비밀번호를 입력하세요.' />
-          <input type="submit" value={"LOGIN"} />
+          <input type="text" placeholder="아이디를 입력하세요." />
+          <input type="password" placeholder="비밀번호를 입력하세요." />
+          <input type="submit" value={'LOGIN'} />
           <p>
-            * 아이디는 환자 번호 혹은 의사 번호이고, 초기 비밀번호는 생년월일입니다. <br/>
-            * 로그인 후 비밀번호를 변경하세요!
+            * 아이디는 환자 번호 혹은 의사 번호이고, 초기 비밀번호는
+            생년월일입니다. <br />* 로그인 후 비밀번호를 변경하세요!
           </p>
         </form>
       </div>
     </LoginBlock>
-  )
+  );
 }
 
-export default Login
+export default Login;
