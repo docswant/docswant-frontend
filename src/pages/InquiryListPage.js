56235d3078f57861b4ff81a49fd0e533129f8c63
Dev-jwJeong <rgn95652@gmail.com>
2022-05-07 18:03:19 +0900

    Fix : 자잘한것 수정

--- src/pages/InquiryListPage.js
+++ src/pages/InquiryListPage.js
@@ -1,14 +1,14 @@
-import React,{useState} from 'react'
-import Header from '../components/common/Header'
-import InquiryListForm from '../components/patient/InquiryListForm'
-import Footer from '../components/common/Footer'
-import Navi from '../components/patient/Navi'
+import React, { useState } from 'react';
+import Header from '../components/common/Header';
+import InquiryListForm from '../components/patient/InquiryListForm';
+import Footer from '../components/common/Footer';
+import Navi from '../components/common/Navi';
 
 function InquiryListPage() {
   const [inquiries, setInquiries] = useState([
     {
       id: 1,
-      text: '안녕하세요'
+      text: '안녕하세요',
     },
   ]);
 
@@ -19,7 +19,7 @@ function InquiryListPage() {
       <InquiryListForm inquiries={inquiries} />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default InquiryListPage
+export default InquiryListPage;
