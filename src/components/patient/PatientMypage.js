e7f338eeecf3ab191243ae593677484d99c27720
Dev-jwJeong <rgn95652@gmail.com>
2022-05-07 18:01:37 +0900

    Add : 환자 페이지

--- src/components/patient/PatientMypage.js
+++ src/components/patient/PatientMypage.js
@@ -1,30 +1,125 @@
 import React from 'react';
+import palette from '../../lib/styles/palette';
 import styled from 'styled-components';
+import { FcSurvey } from 'react-icons/fc';
+import { BiCommentCheck } from 'react-icons/bi';
+import { BiCommentX } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 
 const PatientMypageBlock = styled.div`
   width: 100%;
-  padding: 5rem 10rem;
-
-  .imageBlock {
-    width: 100px;
-    height: 100px;
-    background-color: gray;
-    border-radius: 50%;
-  }
+  padding: 3rem 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   .infoBlock {
+    width: 40%;
+    border: 1px solid ${palette.blue[0]};
+    border-radius: 7px;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
+    padding: 1rem;
+
+    .imgBlock {
+      width: 100px;
+      height: 100px;
+      background-color: gray;
+      border-radius: 50%;
+    }
+    .patientInfo {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      h2 {
+        margin: 0;
+        margin: 0.5rem 0;
+      }
+
+      .patientSubInfo {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 18px;
+      }
+    }
+
+    button {
+      background-color: white;
+      outline: none;
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+      border: 1px solid #dbdbdb;
+      border-radius: 7px;
+      padding: 0.5rem 1rem;
+      font-weight: bold;
+      cursor: pointer;
+    }
+    .subInfo {
+      width: 100%;
+      border-top: 1px solid #dbdbdb;
+      padding: 1rem 3rem;
+      display: flex;
+      justify-content: space-between;
+
+      .totalRequest,
+      .answerRequest,
+      .noAnswerRequest {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        svg {
+          width: 30px;
+          height: 30px;
+          margin-bottom: 0.5rem;
+        }
+      }
+    }
   }
 `;
 
 const PatientMypage = () => {
+  const navigate = useNavigate();
+
+  const onMoveModify = (userId) => {
+    navigate(`/patient/modify/${userId}`);
+  };
   return (
     <PatientMypageBlock>
-      <div className="imageBlock"></div>
       <div className="infoBlock">
-        <h1>환자 : </h1> <h3>정재욱</h3>
-        <h1>환자 : </h1> <h3>정재욱</h3>
+        <div className="imgBlock"></div>
+        <div className="patientInfo">
+          <h2>정재욱</h2>
+          <div className="patientSubInfo">
+            <div style={{ marginRight: '0.3rem', marginBottom: '0.3rem' }}>
+              26세
+            </div>
+            <div>퇴원까지 D-10</div>
+          </div>
+        </div>
+        <button onClick={() => onMoveModify(999)}>설정</button>
+        <div className="subInfo">
+          <div className="totalRequest">
+            <FcSurvey />
+            <b>총 설문</b>
+            <span>10</span>
+          </div>
+          <div className="answerRequest">
+            <BiCommentCheck />
+            <b>답변한 설문</b>
+            <span>3</span>
+          </div>
+          <div className="noAnswerRequest">
+            <BiCommentX />
+            <b>미답변 설문</b>
+            <span>7</span>
+          </div>
+        </div>
       </div>
     </PatientMypageBlock>
   );
---
+++ src/components/patient/PatientSurvey.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const PatientSurveyBlock = styled.div`
+  width: 100%;
+  padding: 3rem 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .surveyList {
+    width: 80%;
+    border-bottom: 1px solid ${palette.blue[0]};
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+
+    .surveyInfo {
+      span {
+        margin-right: 0.5rem;
+        color: #999999;
+      }
+    }
+    .surveyCheck {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .surveyButton {
+        button {
+          margin-left: 1rem;
+          border: 1px solid ${palette.blue[0]};
+          background-color: white;
+          border-radius: 12px;
+          padding: 0.3rem 0.5rem;
+          cursor: pointer;
+
+          &:hover {
+            background-color: ${palette.blue[0]};
+            color: white;
+          }
+        }
+      }
+    }
+  }
+
+  .submitArea {
+    width: 80%;
+    text-align: right;
+
+    button {
+      outline: none;
+      background-color: ${palette.blue[0]};
+      border: none;
+      padding: 1rem 3rem;
+      border-radius: 7px;
+      font-size: 20px;
+      font-weight: bold;
+      margin-top: 1rem;
+      color: white;
+      cursor: pointer;
+    }
+  }
+`;
+
+const PatientSurvey = () => {
+  return (
+    <PatientSurveyBlock>
+      <div className="surveyList">
+        <div className="surveyInfo">
+          <span>김의사</span>
+          <span>2022.05.07</span>
+        </div>
+        <div className="surveyCheck">
+          <p>어디어디 부분은 아직도 아프신가요?</p>
+          <div className="surveyButton">
+            <button>많이 아파요</button>
+            <button>조금 아파요</button>
+            <button>괜찮아요</button>
+            <button>아주 괜찮아요</button>
+          </div>
+        </div>
+      </div>
+      <div className="surveyList">
+        <div className="surveyInfo">
+          <span>김의사</span>
+          <span>2022.05.07</span>
+        </div>
+        <div className="surveyCheck">
+          <p>어디어디 부분은 아직도 아프신가요?</p>
+          <div className="surveyButton">
+            <button>많이 아파요</button>
+            <button>조금 아파요</button>
+            <button>괜찮아요</button>
+            <button>아주 괜찮아요</button>
+          </div>
+        </div>
+      </div>
+      <div className="surveyList">
+        <div className="surveyInfo">
+          <span>김의사</span>
+          <span>2022.05.07</span>
+        </div>
+        <div className="surveyCheck">
+          <p>어디어디 부분은 아직도 아프신가요?</p>
+          <div className="surveyButton">
+            <button>많이 아파요</button>
+            <button>조금 아파요</button>
+            <button>괜찮아요</button>
+            <button>아주 괜찮아요</button>
+          </div>
+        </div>
+      </div>
+      <div className="surveyList">
+        <div className="surveyInfo">
+          <span>김의사</span>
+          <span>2022.05.07</span>
+        </div>
+        <div className="surveyCheck">
+          <p>어디어디 부분은 아직도 아프신가요?</p>
+          <div className="surveyButton">
+            <button>많이 아파요</button>
+            <button>조금 아파요</button>
+            <button>괜찮아요</button>
+            <button>아주 괜찮아요</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="submitArea">
+        <button>설문 제출</button>
+      </div>
+    </PatientSurveyBlock>
+  );
+};
+
+export default PatientSurvey;
