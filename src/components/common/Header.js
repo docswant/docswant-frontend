60310d5988c90a4ec103746b2f8c4ac0b843831d
Dev-jwJeong <rgn95652@gmail.com>
2022-05-07 18:00:44 +0900

    Fix : Header Link 변경

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../lib/image/Logo.png';
 
 const HeaderBlock = styled.div`
-  border-bottom: 1px solid #E1E1E1;
+  border-bottom: 1px solid #e1e1e1;
   padding: 0 2rem;
   display: flex;
   align-items: center;
@@ -16,7 +16,7 @@ const HeaderBlock = styled.div`
     img {
       width: 90%;
       height: 90px;
-      margin: .5rem 0;
+      margin: 0.5rem 0;
       cursor: pointer;
     }
   }
@@ -25,24 +25,19 @@ const HeaderBlock = styled.div`
     font-size: 25px;
     margin-right: 2rem;
   }
-
 `;
 
 const Header = () => {
-  const navigate = useNavigate();
-
   //현재 시간
   const today = new Date();
   const dataString = today.toLocaleDateString();
 
-  //로고 클릭 시 메인페이지 이동
-  const onGoMain = () => {
-    navigate('/');
-  };
   return (
     <HeaderBlock>
       <div className="HeaderLeft">
-        <img src={Logo} alt="Logo" onClick={onGoMain}/>
+        <Link to="/patient/mypage">
+          <img src={Logo} alt="Logo" />
+        </Link>
       </div>
       <span>{dataString}</span>
     </HeaderBlock>
