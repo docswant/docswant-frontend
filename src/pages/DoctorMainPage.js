51e1119070b7f02f52adced41b1548a4afee70cd
Dev-jwJeong <rgn95652@gmail.com>
2022-05-10 00:01:42 +0900

    Add : 의사 메인페이지, 수정 페이지 UI

---
+++ src/pages/DoctorMainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/common/Footer';
+import Header from '../components/common/Header';
+import DoctorMain from '../components/doctor/DoctorMain';
+
+function DoctorMainPage() {
+  return (
+    <div>
+      <Header />
+      <DoctorMain />
+      <Footer />
+    </div>
+  );
+}
+
+export default DoctorMainPage;
