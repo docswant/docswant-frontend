f7a77a3602282c06f5645f2ee38bf3eaae80a7c8
Dev-jwJeong <rgn95652@gmail.com>
2022-05-12 21:15:52 +0900

    Fix : UI 수정

--- src/components/patient/InquiryModifyForm.js
+++ src/components/patient/InquiryModifyForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import { Link } from 'react-router-dom';
@@ -9,10 +9,10 @@ const InquiryModifyBlock = styled.div`
   height: 70vh;
   text-align: center;
   margin: 0 auto;
-  p{
+  p {
     font-size: 30px;
   }
-  textarea{
+  textarea {
     width: 50%;
     height: 80%;
     resize: none;
@@ -20,13 +20,13 @@ const InquiryModifyBlock = styled.div`
     overflow-y: scroll;
     font-size: 18px;
   }
-`
+`;
 const BtnBlock = styled.div`
   background-color: ${palette.gray[2]};
   text-align: right;
   padding-right: 10rem;
   height: 6rem;
-  button{
+  button {
     width: 10rem;
     height: 50%;
     margin-right: 10px;
@@ -35,11 +35,11 @@ const BtnBlock = styled.div`
     font-size: 20px;
     cursor: pointer;
     background-color: ${palette.blue[0]};
-    &:hover{
+    &:hover {
       background-color: ${palette.blue[1]};
     }
   }
-`
+`;
 
 function InquiryModifyForm() {
   return (
@@ -49,15 +49,15 @@ function InquiryModifyForm() {
         <textarea readOnly />
       </InquiryModifyBlock>
       <BtnBlock>
-        <Link to ="/inquiry_list">
+        <Link to="/inquiry_list">
           <button>돌아 가기</button>
         </Link>
-       
+
         <button>수정</button>
         <button>삭제</button>
       </BtnBlock>
     </>
-  )
+  );
 }
 
-export default InquiryModifyForm
+export default InquiryModifyForm;
