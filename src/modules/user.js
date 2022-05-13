8ef24aac545913a9a2dfc673028567b68c397b68
Dev-jwJeong <rgn95652@gmail.com>
2022-05-13 16:28:51 +0900

    Add : user 정보 redux 추가

--- src/modules/user.js
+++ src/modules/user.js
@@ -3,13 +3,19 @@ import { createAction } from 'redux-actions';
 
 const STAGE_USER = 'user/STAGE_USER';
 const STATE_USER_ERROR = 'user/STAGE_USER_ERROR';
+const STAGE_WHO = 'user/STAGE_WHO';
+const STAGE_WHO_ERROR = 'user/STAGE_WHO_ERROR';
 
 export const stageUser = createAction(STAGE_USER, (user) => user);
 export const stageUserError = createAction(STATE_USER_ERROR, (error) => error);
+export const stageWho = createAction(STAGE_WHO, (who) => who);
+export const stageWhoError = createAction(STAGE_WHO_ERROR, (error) => error);
 
 const initialState = {
   user: null,
   userError: null,
+  who: null,
+  whoError: null,
 };
 
 const user = handleActions(
@@ -22,6 +28,15 @@ const user = handleActions(
     [STATE_USER_ERROR]: (state, { payload: error }) => ({
       ...state,
       userError: error,
+    }),
+    [STAGE_WHO]: (state, { payload: who }) => ({
+      ...state,
+      who,
+      whoError: null,
+    }),
+    [STAGE_WHO_ERROR]: (state, { payload: error }) => ({
+      ...state,
+      whoError: error,
     }),
   },
   initialState,
