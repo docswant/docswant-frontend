80dba2cd66700dad60fd0f8be6769c73913b338e
Dev-jwJeong <rgn95652@gmail.com>
2022-05-17 23:49:26 +0900

    Add : 환자 삭제 redux

--- src/modules/patientList.js
+++ src/modules/patientList.js
@@ -3,6 +3,8 @@ import { createAction } from 'redux-actions';
 
 const PATIENT_LIST_SUCCESS = 'patientList/PATIENT_LIST_SUCCESS';
 const PATIENT_LIST_FAILURE = 'patientList/PATIENT_LIST_FAILURE';
+const PATIENT_DELETE_SUCCESS = 'patientList/PATIENT_DELETE_SUCCESS';
+const PATIENT_DELETE_FAILURE = 'patientList/PATIENT_DELETE_FAILURE';
 
 export const patientListSuccess = createAction(
   PATIENT_LIST_SUCCESS,
@@ -12,10 +14,20 @@ export const patientListFailure = createAction(
   PATIENT_LIST_FAILURE,
   (error) => error,
 );
+export const patientDeleteSuccess = createAction(
+  PATIENT_DELETE_SUCCESS,
+  (deletePatient) => deletePatient,
+);
+export const patientDeleteFailure = createAction(
+  PATIENT_DELETE_FAILURE,
+  (error) => error,
+);
 
 const initialState = {
   patientList: null,
   patientListError: null,
+  patientDelete: null,
+  patientDeleteError: null,
 };
 
 const patientList = handleActions(
@@ -28,6 +40,15 @@ const patientList = handleActions(
     [PATIENT_LIST_FAILURE]: (state, { payload: error }) => ({
       ...state,
       patientListError: error,
+    }),
+    [PATIENT_DELETE_SUCCESS]: (state, { payload: patientDelete }) => ({
+      ...state,
+      patientDelete,
+      patientDeleteError: null,
+    }),
+    [PATIENT_DELETE_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      patientDeleteError: error,
     }),
   },
   initialState,
