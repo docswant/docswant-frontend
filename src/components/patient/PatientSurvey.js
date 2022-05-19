6e56a95107a2c9c4514ee173bbce8a421facdd62
Dev-jwJeong <rgn95652@gmail.com>
2022-05-19 23:21:21 +0900

    Fix : 환자 설문 응답 UI 수정

--- src/components/patient/PatientSurvey.js
+++ src/components/patient/PatientSurvey.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import PatientAnswerToDoctorContainer from '../../container/patient/PatientAnswerToDoctorContainer';
 import palette from '../../lib/styles/palette';
 
 const PatientSurveyBlock = styled.div`
   width: 100%;
-  padding: 3rem 10rem;
+  margin: 1rem auto;
+  padding: 1rem 0;
   padding-bottom: 150px;
   display: flex;
   flex-direction: column;
@@ -29,11 +31,10 @@ const PatientSurveyBlock = styled.div`
 
       .surveyButton {
         button {
-          margin-left: 1rem;
           border: 1px solid ${palette.blue[0]};
           background-color: white;
           border-radius: 12px;
-          padding: 0.3rem 0.5rem;
+          padding: 0.5rem 1.5rem;
           cursor: pointer;
 
           &:hover {
@@ -65,30 +66,44 @@ const PatientSurveyBlock = styled.div`
 `;
 
 const PatientSurvey = ({ questionList }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [questionId, setQuestionId] = useState(null);
+  const onOpen = (id) => {
+    setIsOpen(!isOpen);
+    setQuestionId(id);
+  };
   return (
     questionList && (
-      <PatientSurveyBlock>
-        {questionList.content.map((c) => (
-          <div className="surveyList">
-            <div className="surveyInfo">
-              <span>김의사</span>
-              <span>2022.05.07</span>
-            </div>
-            <div className="surveyCheck">
-              <p>{c.content}</p>
-              <div className="surveyButton">
-                <button>많이 아파요</button>
-                <button>조금 아파요</button>
+      <>
+        {isOpen && (
+          <PatientAnswerToDoctorContainer
+            onOpen={onOpen}
+            questionId={questionId}
+          />
+        )}
+        <PatientSurveyBlock>
+          {questionList.content.map((c) => (
+            <div className="surveyList">
+              <div className="surveyInfo">
+                <span>김의사</span>
+                <span>2022.05.07</span>
+              </div>
+              <div className="surveyCheck">
+                <p>{c.content}</p>
+                <div className="surveyButton">
+                  <button onClick={() => onOpen(c.id)}>답변하기</button>
+                  {/* <button>조금 아파요</button>
                 <button>괜찮아요</button>
-                <button>아주 괜찮아요</button>
+                <button>아주 괜찮아요</button> */}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <div className="submitArea">
+          ))}
+          {/* <div className="submitArea">
           <button>설문 제출</button>
-        </div>
-      </PatientSurveyBlock>
+        </div> */}
+        </PatientSurveyBlock>
+      </>
     )
   );
 };
